e562916dd836177547c82bd3740fd9b45dbe43b6
hamza-x <hamza-x@users.noreply.github.com>
2023-03-08 22:24:41 +0000

    Added script to build

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script will create the input file that corex-unit will expect to find
+# and then create an archive including the python scripts and the input file
+
+tar -czf input.tar.gz ore.xml input/
+openssl enc -aes-256-cbc -in input.tar.gz -out input.enc -pass pass:123
+tar -czf corex.tar.gz input.enc corex-unit.py corex.py
